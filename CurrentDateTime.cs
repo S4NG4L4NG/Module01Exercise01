6021bb1704b2e1390344137e1b0b4a3003d96fbf
bmarcko <bobbymarckocruz@gmail.com>
2024-08-21 13:41:57 +0800

    Add project files.

---
+++ CurrentDateTime.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module01Exercise01
+{
+    public class CurrentDateTime : IMarkupExtension
+    {
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return $"Welcome User The Current Date and Time of Today is {DateTime.Now:MMMM dd, yyyy}";
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace Module01Exercise01
+{
+    public partial class MainPage : ContentPage
+    {
+        
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void UpdateInfo(object sender, EventArgs e)
+        {
+            await DisplayAlert("Alert", "Info Updated", "ok");
+        }
+
+        private void OnChangeBackgroundColor(object sender, EventArgs e)
+        {
+            this.Resources["LocalBackgroundColor"] = Colors.LightBlue;
+        }
+
+        private void OnChangeBackgroundColor1(object sender, EventArgs e)
+        {
+            this.Resources["LocalBackgroundColor"] = Colors.DarkBlue;
+        }
+
+
+    }
+
+}
